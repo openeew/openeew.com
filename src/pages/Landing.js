41cc4c429020103a8c19e67c559d8bdf7dc8baee
Ryan Kelley <rdkelley@users.noreply.github.com>
2021-05-26 12:57:16 -0700

    Add footer, revise photo
    Signed-off-by: Ryan Kelley <rdkelley@users.noreply.github.com>

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import { SensorInfo, DashboardInfo, NetworkInfo } from '../components/Sensors';
 import Banner from '../components/Banner';
 import Diagram from '../components/Diagram';
 import WhoWeAre from '../components/WhoWeAre';
 
+import AppContext from '../context/app';
+
 const Landing = () => {
+  const { t } = useContext(AppContext);
+
   return (
     <div>
       <Header />
@@ -15,7 +19,19 @@ const Landing = () => {
       <SensorInfo />
       <DashboardInfo />
       <NetworkInfo />
-      {/* <div className="landing__footer"></div> */}
+      <div className="landing__footer">
+        <h3>
+          OpenEEW is a{' '}
+          <a
+            target="_blank"
+            href="https://www.linuxfoundation.org/projects/call-for-code"
+          >
+            Call for Code® with The Linux Foundation
+          </a>{' '}
+          project
+        </h3>
+        <h4>{t('copyright')}</h4>
+      </div>
     </div>
   );
 };
